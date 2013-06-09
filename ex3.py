146562fca4c182787f38efc8e08d7e2229d26007
Lukas Kitsche <lu.kitsche@gmail.com>
2013-06-09 18:49:07 +0200

    Exercise 3: Numbers And Math

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# printing some text
+print "I will now count my chickens:"
+
+# printing some text and the result of the following calculation
+print "Hens", 25 + 30 / 6.0
+# printing some text and the result of the following calculation again
+print "Roosters", 100 - 25 * 3.0 % 4
+
+# printing some text again
+print "Now I will count the eggs:"
+
+# printing the result of the following calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+# printing some text again
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# printing if the following expression is 'True' of 'False'
+print 3 + 2 < 5 - 7
+
+# printing some text and the result of the following calculation
+print "What is 3 + 2?", 3 + 2
+# printing some text and the result of the following calculation
+print "What is 5 - 7?", 5 - 7
+
+# printing some text
+print "Oh, that's why it's False."
+
+# printing some text again
+print "How about some more."
+
+# printing some text and if the following expression is 'True' of 'False'
+print "Is it greater?", 5 > -2
+# printing some text and if the following expression is 'True' of 'False' again
+print "Is it greater or equal?", 5 >= -2
+# printing some text and if the following expression is 'True' of 'False' again
+print "Is it less or equal?", 5 <= -2
---
+++ ex3_calc.py
@@ -0,0 +1,5 @@
+#some calculation
+print "What's the area of a circle with the radius 5cm?", 3.14159265358979323846264 * 5 * 5
+
+print "Is 42 the answer to all?", 42 == True
+print "Fascinating"
