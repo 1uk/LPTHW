3787445c5ac829bdea6b0c70fa5f146306869565
Lukas Kitsche <lu.kitsche@gmail.com>
2013-06-10 17:09:26 +0200

    Exercise 7: More Printing

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# printing a string
+print "Mary had a little lamb."
+# printing a string with an included string
+print "Its fleece was white as %s." % 'snow'
+# printing a string
+print "And everywhere that Mary went."
+# printing 10 times "."
+print "." * 10 # what'd that do?
+
+# defining some strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# removing it causes that there is a newline between end6 and end7
+# printing some letters (strings)
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
