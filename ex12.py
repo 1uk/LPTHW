86e868efedb1fdd0ead2692abca1d9764f1d1d1a
Lukas Kitsche <lu.kitsche@gmail.com>
2013-06-11 23:13:00 +0200

    Exercise 12: Prompting People

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+name = raw_input("And what's your name? ")
+teeth = raw_input("How many teeths do you have? ")
+print "WHAAAT, you don't that ? shame on you, %s !" % name
