320169dfec0eeea59a0a64b8456111be91393042
Lukas Kitsche <lu.kitsche@gmail.com>
2013-06-11 16:15:15 +0200

    Exercise 10: What Was That?

---
+++ ex10_plar.py
@@ -0,0 +1,21 @@
+# playing around
+
+backslash = '\\'
+single_quote = '\''
+double_quote = '\"'
+ascii_bell = '\a'
+ascii_backspace = '\b'
+ascii_formfeed = '\f'
+ascii_linefeed = '\n'
+unicode_name = '\N{name}'
+carriage_return = '\r'
+horizontal_tab = '\t'
+hex_16bit = '\uxxxx'
+hex_32bit = '\Uxxxxxxxx'
+vertical_tab = '\v'
+octal_char_ooo = '\ooo'
+octal_char_hh = '\xhh'
+
+
+print "Hello guys, want to see a show ? Then you wrong here !"
+print "Here comes the Escape Sequence Table !"
