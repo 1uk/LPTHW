6d6d860955bebe916d1e10fde2ca7cedc3250f55
Lukas Kitsche <lu.kitsche@gmail.com>
2013-07-01 22:37:32 +0200

    Exercise 18: Names, Variables, Code, Functions

---
+++ checklist_call.py
@@ -0,0 +1,11 @@
+def checklist():
+    print "1. Did you call/use/run this function by typing its name?",
+    raw_input("[Y/n]")
+    print "2. Did you put ( character after the name to run it?",
+    raw_input("[Y/n]")
+    print "3. Did you put the values you want into the parenthesis separated by commas?",
+    raw_input("[Y/n]")
+    print "4. Did you end the function call with a ) character?",
+    raw_input("[Y/n]")
+
+checklist()
---
+++ checklist_function.py
@@ -0,0 +1,19 @@
+def checklist():
+    print "1. Did you start your function definition with def?",
+    raw_input("[Y/n]")
+    print "2. Does your funcion name have only characters and _ (underscore) characters?",
+    raw_input("[Y/n]")
+    print "3. Did you put an open parenthesis ( right after the function name?",
+    raw_input("[Y/n]")
+    print "4. Did you put your arguments after the parenthesis ( separated by commas?",
+    raw_input("[Y/n]")
+    print "5. Did you make each argument unique (meaning no duplicated names).",
+    raw_input("[Y/n]")
+    print "6. Did you put a close parenthesis and a colon ): after the arguments?",
+    raw_input("[Y/n]")
+    print "7. Did you indent all lines of code you want in the function 4 spaces? No more, no less.",
+    raw_input("[Y/n]")
+    print '8. Did you "end" your function by going back to writing with no indent (dedenting we call it)?',
+    raw_input("[Y/n]")
+
+checklist()
