f09ca488365ace511419bbaad9eabb33d6b4b030
Lukas Kitsche <lu.kitsche@gmail.com>
2013-09-12 21:19:17 +0200

    Exercise 22: 'The List'

---
+++ ex22.py
@@ -0,0 +1,12 @@
+def # defines a function
+<indent> # blocks, e.g. function declaration
+print # printing out some text
+return # returning value(s)!
+:   #  when defining a function
+=   # arithmetic
+# for comments
+"""
+string
+""" # for multiline strings
+from # module
+import # object
